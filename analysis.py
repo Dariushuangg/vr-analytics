584c336de0d746b460c15a1d28033e16b9bcbefd
Darius_sh711 <sh711@duke.edu>
2022-11-25 19:32:31 -0500

    refactored out analysis.py and created some function prototypes

---
+++ analysis.py
@@ -0,0 +1,24 @@
+from parse import parse
+from clean import *
+
+def analyze():
+    """Entrance function for generating analytic results.
+    """
+    pass
+
+def concentration(file: pd.DataFrame) -> None:
+    """
+    Analyze the test subject's level of concentration throughout time
+    Args:
+        file (pd.DataFrame): cleaned dataframe representing a file from experiment
+    """
+    pass
+
+def completion_time(file: pd.DataFrame) -> None:
+    """
+    Analyze the time spent on completing the task
+    Args:
+        file (pd.DataFrame): _description_
+    """
+    pass
+
